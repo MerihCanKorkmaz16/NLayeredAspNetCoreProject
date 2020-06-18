ee2f06fd65306269ea92840fae1c242d5bb94fc4
Merih Can Korkmaz <merihcankorkmaz16@gmail.com>
2020-06-18 16:27:03 +0300

    Katmanlar ve Repository Desing pattern

---
+++ NLayeredProjectExample/NLayeredProjectExample.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using NLayeredProjectExample.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayeredProjectExample.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        T Add(T entity);
+        Task<T> AddAsync(T entity);
+        T Update(T entity);
+        Task<T> UpdateAsync(T entity);
+        void Delete(T entity);
+        Task DeleteAsync(T entity);
+        T Get(Expression<Func<T,bool>> filter = null);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+    }
+}
---
+++ NLayeredProjectExample/NLayeredProjectExample.Core/Entities/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayeredProjectExample.Core.Entities
+{
+    public interface IEntity
+    {
+    }
+}
